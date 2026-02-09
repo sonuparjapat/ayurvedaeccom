291223af6db48e27d71356ddbb823319604c0fbf
sonuparjapat <sahil992001parjapat@gmail.com>
2026-02-09 21:19:25 +0530

    change

--- backend/src/config/mail.js
+++ backend/src/config/mail.js
@@ -3,7 +3,11 @@ const nodemailer = require("nodemailer")
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false, // TLS
+  secure: false,
+  requireTLS: true,
+  connectionTimeout: 10000, // 10 sec
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
