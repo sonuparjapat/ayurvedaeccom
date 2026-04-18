8e2be87244255bed6c02b5d9c6ad2e23da7ef2c8
sonuparjapat <sahil992001parjapat@gmail.com>
2026-04-19 03:00:54 +0530

    chagnes

---
+++ backend/src/utils/getMimeType.js
@@ -0,0 +1,13 @@
+export const getMimeType = (fileName = '') => {
+  const ext = path
+    .extname(fileName)
+    .toLowerCase()
+
+  if (ext === '.png')
+    return 'image/png'
+
+  if (ext === '.webp')
+    return 'image/webp'
+
+  return 'image/jpeg'
+}
