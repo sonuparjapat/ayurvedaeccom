b1ebb32c8d502227168fafc701e65e626d6bbcd8
sonuparjapat <sahil992001parjapat@gmail.com>
2026-07-30 15:34:28 +0530

    changes

--- backend/src/modules/quiz/quiz.routes.js
+++ backend/src/modules/quiz/quiz.routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const ctrl = require('./quiz.controller')
 
-
+// console.log("Hii")
 // Public — no auth required to take quiz or submit
 router.get('/questions', ctrl.getQuestions)
 router.post('/result', ctrl.submitResult)
